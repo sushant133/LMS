8e34b17e65ba89d51b9f76150154a0041785b25e
Sushant Mahato <sushantmahato133@gmail.com>
2026-07-29 22:48:41 +0545

    last2

---
+++ frontend/android/app/src/main/java/np/com/phit/lms2/MainActivity.java
@@ -0,0 +1,5 @@
+package np.com.phit.lms2;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'np.com.phit.lms2',
+  appName: 'PHIT COLLEGE',
+  webDir: 'dist'
+};
+
+export default config;
